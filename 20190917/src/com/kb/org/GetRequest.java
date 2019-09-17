0ae6c069d2e18e68089d96a3c3610061ce3a5f32
305 <305@305-PC>
2019-09-17 16:39:39 +0900

    JSP

---
+++ 20190917/src/com/kb/org/GetRequest.java
@@ -0,0 +1,40 @@
+package com.kb.org;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/GetRequest")
+public class GetRequest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public GetRequest() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html charset=UTF-8");
+		response.setCharacterEncoding("utf-8");
+
+		String name = request.getParameter("aaa");
+
+		PrintWriter out = response.getWriter();
+		
+		out.println("<h2>이 요청은 doPost에서 처리되었습니다.</h2><br>");
+		out.println("<h2>name : [" + name + "]</h2>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
